b7be575d8ef6901d2120e0f70d6d4217001f25b4
Kazuki Ota <kaota@microsoft.com>
2020-12-05 19:54:57 +0900

    プロジェクト ファイルを追加します。

---
+++ Issue212/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+using System.ComponentModel;
+using Xunit;
+
+namespace Issue212
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Board_Has_Observable()
+        {
+            var rt = new ReactiveThing();
+            rt.Model.Update();
+            rt.Model.Update();
+
+            Assert.Equal(2, rt.RpInt.Value);
+        }
+    }
+
+    class SomeModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private int _someInt;
+        public int SomeInt => _someInt;
+
+        public void Update()
+        {
+            _someInt++;
+            PropertyChanged?.Invoke(this, new(nameof(SomeInt)));
+        }
+    }
+
+    class ReactiveThing
+    {
+        public ReadOnlyReactiveProperty<int> RpInt { get; }
+        public SomeModel Model { get; }
+        public ReactiveThing()
+        {
+            Model = new SomeModel();
+            RpInt = Model.ObserveProperty(x => x.SomeInt)
+                .ToReadOnlyReactiveProperty();
+        }
+    }
+}
